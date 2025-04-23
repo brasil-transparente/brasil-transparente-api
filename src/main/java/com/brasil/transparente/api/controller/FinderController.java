76629071fb86df0af53689b0f9ee9b05af20adf1
WeimarJr <weimarvlademirojunior@gmail.com>
2025-04-23 17:01:57 -0400

    refatoração:mundaça de injeção de @Autowire para injeção por construtor, ajustes nas rotas do controller, exclusão de imports que ja não eram necessarios

--- src/main/java/com/brasil/transparente/api/controller/FinderController.java
+++ src/main/java/com/brasil/transparente/api/controller/FinderController.java
@@ -3,9 +3,8 @@ package com.brasil.transparente.api.controller;
 import com.brasil.transparente.api.dto.DisplayableElementDTO;
 import com.brasil.transparente.api.entity.*;
 import com.brasil.transparente.api.service.FinderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,40 +12,44 @@ import java.util.List;
 @RestController
 public class FinderController {
 
-    @Autowired
-    private FinderService finderService;
 
-    @GetMapping("/getPoderes")
+    private FinderService finderService;
+    public FinderController( FinderService finderService)
+    {
+        this.finderService = finderService;
+    }
+
+    @GetMapping("/poderes")
     public List<DisplayableElementDTO> getPoderList() {
         return finderService.getPoderList();
     }
 
-    @GetMapping("/getMinisterioByPoderId")
-    public List<DisplayableElementDTO> getMinisterioByPoderId(@RequestParam Long poderId) {
+    @GetMapping("/poder/{idPoder}/ministerios")
+    public List<DisplayableElementDTO> getMinisterioByPoderId(@PathVariable("idPoder") Long poderId) {
         return finderService.getMinisterioByPoderId(poderId);
     }
 
-    @GetMapping("/getOrgaoByMinisterioId")
-    public List<DisplayableElementDTO> getOrgaoByMinisterioId(@RequestParam Long ministerioId) {
+    @GetMapping("/ministerio/{idMinisterio}/orgaos")
+    public List<DisplayableElementDTO> getOrgaoByMinisterioId(@PathVariable("idMinisterio") Long ministerioId) {
         return finderService.getOrgaoByMinisterioId(ministerioId);
     }
 
-    @GetMapping("/getUnidadeGestoraByOrgaoId")
-    public List<DisplayableElementDTO> getUnidadeGestoraByOrgaoId(@RequestParam Long orgaoId) {
+    @GetMapping("/orgao/{idOrgao}/unidades-gestoras")
+    public List<DisplayableElementDTO> getUnidadeGestoraByOrgaoId(@PathVariable("idOrgao") Long orgaoId) {
         return finderService.getUnidadeGestoraByOrgaoId(orgaoId);
     }
 
-    @GetMapping("/getElementoDespesaByUnidadeGestoraId")
-    public List<DisplayableElementDTO> getElementoDespesaByUnidadeGestoraId(@RequestParam Long unidadeGestoraId) {
+    @GetMapping("/unidade-gestora/{idUnidadeGestora}/elemento-despesa")
+    public List<DisplayableElementDTO> getElementoDespesaByUnidadeGestoraId(@PathVariable("idUnidadeGestora") Long unidadeGestoraId) {
         return finderService.getElementoDespesaByUnidadeGestoraId(unidadeGestoraId);
     }
 
-    @GetMapping("/getSimplifiedReport")
+    @GetMapping("/despesa-simplificada")
     public List<DespesaSimplicada> getSimplifiedReport() {
         return finderService.getSimplifiedReport();
     }
 
-    @GetMapping("/getGastoTotal")
+    @GetMapping("/gasto-total")
     public Double getGastoTotal() {
         return finderService.getGastoTotal();
     }
