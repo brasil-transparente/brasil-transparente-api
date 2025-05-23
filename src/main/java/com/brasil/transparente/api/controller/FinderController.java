efa776ec055957f2d71cf81931a0368da55261f4
Diego Miranda <5113722+diegoomiranda@users.noreply.github.com>
2025-05-23 19:19:47 -0300

    Integração do Swagger via springdoc-openapi

--- src/main/java/com/brasil/transparente/api/controller/FinderController.java
+++ src/main/java/com/brasil/transparente/api/controller/FinderController.java
@@ -3,53 +3,59 @@ package com.brasil.transparente.api.controller;
 import com.brasil.transparente.api.dto.DisplayableElementDTO;
 import com.brasil.transparente.api.entity.*;
 import com.brasil.transparente.api.service.FinderService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.*;
+
+@OpenAPIDefinition(info = @Info(title = "API REST Brasil Transparente", version = "1.0.0"))
+@Tag(name = "Busca")
 @RestController
 public class FinderController {
 
     private final FinderService finderService;
 
-    public FinderController( FinderService finderService)
-    {
+    public FinderController(FinderService finderService) {
         this.finderService = finderService;
     }
 
-    @GetMapping("/unidade-federativa/{unidadeFederativaId}/poderes")
+    @GetMapping(value = "/unidade-federativa/{unidadeFederativaId}/poderes", produces = APPLICATION_JSON_VALUE)
     public List<DisplayableElementDTO> getPoderesByUnidadeFederativa(@PathVariable("unidadeFederativaId") Long unidadeFederativaId) {
         return finderService.getPodereByUnidadeFederativa(unidadeFederativaId);
     }
 
-    @GetMapping("/poder/{idPoder}/ministerios")
+    @GetMapping(value = "/poder/{idPoder}/ministerios", produces = APPLICATION_JSON_VALUE)
     public List<DisplayableElementDTO> getMinisterioByPoderId(@PathVariable("idPoder") Long poderId) {
         return finderService.getMinisterioByPoderId(poderId);
     }
 
-    @GetMapping("/ministerio/{idMinisterio}/orgaos")
+    @GetMapping(value = "/ministerio/{idMinisterio}/orgaos", produces = APPLICATION_JSON_VALUE)
     public List<DisplayableElementDTO> getOrgaoByMinisterioId(@PathVariable("idMinisterio") Long ministerioId) {
         return finderService.getOrgaoByMinisterioId(ministerioId);
     }
 
-    @GetMapping("/orgao/{idOrgao}/unidades-gestoras")
+    @GetMapping(value = "/orgao/{idOrgao}/unidades-gestoras", produces = APPLICATION_JSON_VALUE)
     public List<DisplayableElementDTO> getUnidadeGestoraByOrgaoId(@PathVariable("idOrgao") Long orgaoId) {
         return finderService.getUnidadeGestoraByOrgaoId(orgaoId);
     }
 
-    @GetMapping("/unidade-gestora/{idUnidadeGestora}/elemento-despesa")
+    @GetMapping(value = "/unidade-gestora/{idUnidadeGestora}/elemento-despesa", produces = APPLICATION_JSON_VALUE)
     public List<DisplayableElementDTO> getElementoDespesaByUnidadeGestoraId(@PathVariable("idUnidadeGestora") Long unidadeGestoraId) {
         return finderService.getElementoDespesaByUnidadeGestoraId(unidadeGestoraId);
     }
 
-    @GetMapping("/despesa-simplificada/{unidadeFederativaId}")
+    @GetMapping(value = "/despesa-simplificada/{unidadeFederativaId}", produces = APPLICATION_JSON_VALUE)
     public List<DespesaSimplificada> getSimplifiedReport(@PathVariable("unidadeFederativaId") Long unidadeFederativaId) {
         return finderService.getSimplifiedReport(unidadeFederativaId);
     }
 
-    @GetMapping("/unidade-federativa/{unidadeFederativaId}/total-value-spent")
+    @GetMapping(value = "/unidade-federativa/{unidadeFederativaId}/total-value-spent", produces = APPLICATION_JSON_VALUE)
     public Double getTotalValueSpentByUnidadeFederativaId(@PathVariable("unidadeFederativaId") Long unidadeFederativaId) {
         return finderService.getTotalValueSpentByUnidadeFederativaId(unidadeFederativaId);
     }
